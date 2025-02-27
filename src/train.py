14b1a037b6276c6be75cd94582ccbe29f55bbddc
bimbo-22 <bimbimuhad@gmail.com>
2025-02-27 09:47:46 +0100

    tracking metrics

--- src/train.py
+++ src/train.py
@@ -7,15 +7,21 @@ import os
 from dotenv import load_dotenv
 import yaml
 import shutil
-from mlflow.exceptions import MlflowExceptions 
+from dotenv import load_dotenv
+load_dotenv()
+
 
 params = yaml.safe_load(open('params.yaml'))['train']
 # params = yaml.safe_load(open('params.yaml'))['preprocess']
 
+mlflow_tracking_uri = os.getenv("MLFLOW_TRACKING_URI")
+mlflow_tracking_username = os.getenv("MLFLOW_TRACKING_USERNAME")
+mlflow_tracking_password = os.getenv("MLFLOW_TRACKING_PASSWORD")
 
             
             
 def train_model(model_path, epochs, batch):
+    mlflow.set_tracking_uri(mlflow_tracking_uri)
     with mlflow.start_run():
         mlflow.log_param("dataset", "COCO")
         mlflow.log_param("model_used", model_path)
@@ -24,7 +30,28 @@ def train_model(model_path, epochs, batch):
         mlflow.log_param("batch_size", batch)
         
         model = YOLO(model_path)
-        model.train(data=coco128.yaml, epochs=epochs, imgsz=640, batch=batch)
+        best_model = model.train(data=coco128.yaml, epochs=epochs, imgsz=640, batch=batch)
+        
+        mlflow.log_metric("mAP", best_model.results["mAP"])
+        mlflow.log_metric("P", best_model.results["P"])
+        mlflow.log_metric("R", best_model.results["R"])
+        mlflow.log_metric("F1", best_model.results["F1"])
+        mlflow.log_metric("val_loss", best_model.results["val_loss"])
+        mlflow.log_metric("train_loss", best_model.results["train_loss"])
+        
+        tracking_uri_type_store = urlparse(mlflow_tracking_uri).scheme
+        
+        if tracking_uri_type_store != "file":
+            try:
+                registered_model = mlflow.register_model(best_model, "COCO Trained Model")
+                print("Model registered: ", registered_model)
+            except mlflow.MlflowException as e:
+                print(f"Model registration failed: {e}")
+        else:
+            mlflow.register_model(best_model, "COCO Trained Model")
+            print("Model not registered as tracking uri is file")
+        
+        
 
 
    
