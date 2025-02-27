9b5d06942e73bcfb1a60979066b7496e65e3fdaa
bimbo-22 <bimbimuhad@gmail.com>
2025-02-27 09:17:28 +0100

    updated train.py

--- src/train.py
+++ src/train.py
@@ -1,27 +1,51 @@
 from ultralytics import YOLO
+import pickle
 from mlflow.models import infer_signature
 import mlflow
 from urllib.parse import urlparse
 import os
 from dotenv import load_dotenv
 import yaml
+import shutil
+from mlflow.exceptions import MlflowExceptions 
 
 params = yaml.safe_load(open('params.yaml'))['train']
 # params = yaml.safe_load(open('params.yaml'))['preprocess']
 
 
+            
+            
+def train_model(model_path, epochs, batch):
+    with mlflow.start_run():
+        mlflow.log_param("dataset", "COCO")
+        mlflow.log_param("model_used", model_path)
+        mlflow.log_param("epochs", epochs)
+        mlflow.log_param("img_size", 640) 
+        mlflow.log_param("batch_size", batch)
+        
+        model = YOLO(model_path)
+        model.train(data=coco128.yaml, epochs=epochs, imgsz=640, batch=batch)
 
 
-def train(data_path,model_path):
-    model = YOLO(model_path)
-    print("model info: ",model.info())
-    results = model.train(data=data_path, epochs=1, imgsz=640)
-    return results
-    
+   
 
 if __name__ == "__main__":
     print("Script is executing..........")
-    results = train(params['data'],params['model']) 
+    results = train_model(params['model'], 50, 16) 
     print("results: ",results)
 
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
