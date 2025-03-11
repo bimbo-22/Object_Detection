f69afa6997e8f3eae7d8b0561fdabaab421136ab
bimbo-22 <bimbimuhad@gmail.com>
2025-03-11 06:12:31 +0000

    updated optimize.py

--- src/optimisize.py
+++ src/optimisize.py
@@ -16,15 +16,16 @@ mlflow_tracking_uri = os.getenv("MLFLOW_TRACKING_URI")
 mlflow_tracking_username = os.getenv("MLFLOW_TRACKING_USERNAME")
 mlflow_tracking_password = os.getenv("MLFLOW_TRACKING_PASSWORD")
 
+mlflow.set_tracking_uri(mlflow_tracking_uri)
 
 def objective(trial):
     
     lr0 = trial.suggest_float("lr0", 1e-5, 1e-2, log=True)
     lrf = trial.suggest_float("lrf", 1e-5, 1e-1, log=True)
     epochs = trial.suggest_categorical("epochs", [10, 15, 25, 50])
-    batch_size = trial.suggest_categorical("batch_size", [8, 16, 32, 64])
+    batch_size = trial.suggest_categorical("batch_size", [8, 16, 32])
     optimizer = trial.suggest_categorical("optimizer", ["SGD", "Adam", "AdamW"])
-    imgsz = trial.suggest_categorical("imgsz", [512, 640, 768, 896])
+    imgsz = trial.suggest_categorical("imgsz", [512, 640, 768])
     warmup_epochs = trial.suggest_int("warmup_epochs", 0, 5)
     momentum = trial.suggest_float("momentum", 0.85, 0.95)
     weight_decay = trial.suggest_float("weight_decay", 1e-6, 1e-2)
@@ -40,50 +41,57 @@ def objective(trial):
 
     for param in model.model.parameters():
         param.requires_grad = True
-    
-    results = model.train(
-        data = params['data'],
-        epochs = epochs,
-        batch = batch_size,
-        imgsz = imgsz,
-        lr0 = lr0,
-        lrf = lrf,
-        optimizer = optimizer,
-        warmup_epochs = warmup_epochs,
-        momentum = momentum,
-        weight_decay = weight_decay,
-        mosaic = mosaic,
-        mixup = mixup,
-        hsv_h = hsv_h,
-        hsv_s = hsv_s,
-        hsv_v = hsv_v,
-        device = 'cuda' if torch.cuda.is_available() else 'cpu',
-        project = "optimizing_cctv_model",
-        name = f'trial_{trial.number}',
-        save = True,
-        exist_ok = True
-    )
-        
-    mAP50 = results.box.map50
-    return mAP50
+
+    run_name = f"trial_{trial.number}_lr0_{lr0:.2e}_epochs_{epochs}"
+
+    with mlflow.start_run(run_name=run_name):
+        results = model.train(
+            data = params['data'],
+            epochs = epochs,
+            batch = batch_size,
+            imgsz = imgsz,
+            lr0 = lr0,
+            lrf = lrf,
+            optimizer = optimizer,
+            warmup_epochs = warmup_epochs,
+            momentum = momentum,
+            weight_decay = weight_decay,
+            mosaic = mosaic,
+            mixup = mixup,
+            hsv_h = hsv_h,
+            hsv_s = hsv_s,
+            hsv_v = hsv_v,
+            device = 'cuda' if torch.cuda.is_available() else 'cpu',
+            project = "optimizing_cctv_model_v1",
+            name = f'trial_{trial.number}',
+            save = True,
+            exist_ok = True
+        )
+            
+        mAP50 = results.box.map50
+        return mAP50
     
 def optimize(n_trials=20):
-    study = optuna.create_study(direction="maximize", study_name="yolov8_hyperparameter_optimization",
-    pruner = optuna.pruners.MedianPruner(n_warmup_steps=3),
+    # Set the MLflow experiment name explicitly
+    experiment_name = "optimizing_cctv_model_v1"
+    mlflow.set_experiment(experiment_name)
+
+    # Create an Optuna study
+    study = optuna.create_study(
+        direction="maximize",
+        study_name=experiment_name,
+        pruner=optuna.pruners.MedianPruner(n_warmup_steps=3),
     )
-    
-    mlflow_callback =  MLflowCallback(
-        tracking_uri = mlflow_tracking_uri,
-        metric_name = "mAP50",
-    )
-    
-    study.optimize(objective, n_trials=n_trials, callbacks=[mlflow_callback])
-    
+
+    # Optimize the objective function
+    study.optimize(objective, n_trials=n_trials)
+
+    # Print results
     print("Best trial:", study.best_trial.number)
     print("Best parameters:", study.best_params)
     print("Best mAP50:", study.best_value)
-    
-    return  study.best_params
+
+    return study.best_params
 
 if __name__ == "__main__":
     print("Script is executing..........")
